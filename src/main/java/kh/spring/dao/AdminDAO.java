80f011a9a2521f38b2898b2da83639d32ddefd75
yongjunyoo <yongjunyoo.dev@gmail.com>
2022-01-14 17:36:45 +0900

    용준:boardMain,boardDetail modified, AdminController,AdminDAO,AdminService,adminMain added

--- src/main/java/kh/spring/dao/AdminDAO.java
+++ src/main/java/kh/spring/dao/AdminDAO.java
@@ -1,20 +1,12 @@
 package kh.spring.dao;
 
-import java.util.List;
-
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import kh.spring.dto.BoardDTO;
-
 @Repository
-public class BoardDAO {
+public class AdminDAO {
 	
 	@Autowired
 	private SqlSessionTemplate mybatis;
-	
-	public List<BoardDTO> selectList(){
-		return mybatis.selectList("Board.selectAll");
-	}
 }
