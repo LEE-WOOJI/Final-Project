beb52612621fdf32aab898f85faabe170fc9fe46
danpyeon19 <danpyeon19@naver.com>
2022-01-27 15:36:54 +0900

    Admin controller rollback

--- src/main/java/kh/spring/controller/AdminController.java
+++ src/main/java/kh/spring/controller/AdminController.java
@@ -1,6 +1,13 @@
 package kh.spring.controller;
 
 import java.util.List;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,9 +18,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 
+import kh.spring.dto.AdminUtilsDTO;
+import kh.spring.dto.BoardDTO;
+import kh.spring.dto.ChalDTO;
 import kh.spring.dto.MemberDTO;
 import kh.spring.dto.RefundDTO;
 import kh.spring.service.AdminService;
+import kh.spring.service.BoardService;
 import kh.spring.service.RefundService;
 import kh.spring.service.UserService;
 
@@ -25,10 +36,175 @@ public class AdminController {
 	AdminService aService;
 	
 	@Autowired
+	BoardService bService;
+	
+	@Autowired
 	UserService uService;
 	
 	@Autowired
 	RefundService rService;
+
+	@RequestMapping("main") // 메인으로 이동.
+	public String main(Model model) {
+		// 회원 수 출력.
+		int memberResult = aService.getMemberCount();
+		// 자유게시판 글 수 출력.
+		int boardResult = aService.getBoardCount();
+		// 챌린지 수 출력.
+		int chalResult = aService.getChalCount();
+		// 등급 출력.
+		AdminUtilsDTO gradeResult = aService.getGradeCount();
+
+		model.addAttribute("memberResult",memberResult);
+		model.addAttribute("boardResult",boardResult);
+		model.addAttribute("chalResult",chalResult);
+		model.addAttribute("gradeResult",gradeResult);
+		return "/admin/adminMain";
+	}
+	
+	@RequestMapping("board") // 자유게시판 관리로 이동.
+	public String board(Model model, int cpage) throws Exception {
+		// 자유게시판 글 수 출력.
+		int boardResult = aService.getBoardCount();
+		// 자유게시판 글 목록 출력.
+		Map<String,String> map = bService.pageCheck(cpage);
+		int currentPage = Integer.parseInt(map.get("currentPage"));
+		int start = Integer.parseInt(map.get("start"));
+		int end = Integer.parseInt(map.get("end"));
+
+		String navi = aService.getBoardPageNavi(currentPage);
+		List<BoardDTO> list = bService.selectAll(start,end);
+		model.addAttribute("cpage",cpage);
+		model.addAttribute("navi",navi);
+		model.addAttribute("list",list);
+		model.addAttribute("boardResult",boardResult);
+		return "/admin/adminBoard";
+	}
+
+	@RequestMapping("boardSearch") // 자유게시판 관리에서 글 검색.
+	public String boardSearch(Model model, int cpage, String select, String keyword) throws Exception {
+		Map<String,String> map = bService.pageCheck(cpage);
+		int currentPage = Integer.parseInt(map.get("currentPage"));
+		int start = Integer.parseInt(map.get("start"));
+		int end = Integer.parseInt(map.get("end"));
+
+		String navi = aService.getBoardPageNaviSearch(currentPage,select,keyword);
+		List<BoardDTO> list = bService.selectAllSearch(start,end,select,keyword);
+		model.addAttribute("cpage",cpage);
+		model.addAttribute("select",select);
+		model.addAttribute("keyword",keyword);
+		model.addAttribute("navi",navi);
+		model.addAttribute("list",list);
+		return "/admin/adminBoardSearch";
+	}
+
+	@RequestMapping("boardDelete") // 자유게시판 관리에서 글 삭제.
+	public String boardDelete(Model model, int cpage, String select, String keyword, int[] checkbox) {
+		for(int i=0; i<checkbox.length; i++) {
+			bService.delete(checkbox[i]);
+		}
+		model.addAttribute("cpage",cpage);
+		return "redirect:/admin/board";
+	}
+
+	@RequestMapping("chal") // 챌린지 관리로 이동.
+	public String chal(Model model, int cpage) throws Exception {
+		// 챌린지 수 출력.
+		int chalResult = aService.getChalCount();
+		// 챌린지 목록 출력.
+		Map<String,String> map = aService.chalPageCheck(cpage);
+		int currentPage = Integer.parseInt(map.get("currentPage"));
+		int start = Integer.parseInt(map.get("start"));
+		int end = Integer.parseInt(map.get("end"));
+
+		String navi = aService.getChalPageNavi(currentPage);
+		List<ChalDTO> list = aService.selectChalAll(start,end);
+		for(int i=0; i<list.size(); i++) {
+			Timestamp endDate = list.get(i).getEndDate();
+			int chalSeq = list.get(i).getChalSeq();
+			Long datetime = System.currentTimeMillis();
+			Timestamp timestamp = new Timestamp(datetime);
+			String chalStat = "";
+			if(timestamp.getTime()-endDate.getTime()<0) {
+				chalStat ="진행중";
+				aService.updateChalStatus(chalSeq,chalStat);
+			}else {
+				chalStat ="만료";
+				aService.updateChalStatus(chalSeq,chalStat);
+			}
+		}
+		model.addAttribute("cpage",cpage);
+		model.addAttribute("navi",navi);
+		model.addAttribute("list",list);
+		model.addAttribute("chalResult",chalResult);
+		return "/admin/adminChal";
+	}
+
+	@RequestMapping("chalSearch") // 챌린지 관리에서 글 검색.
+	public String chalSearch(Model model, int cpage, String select, String keyword) throws Exception {
+		Map<String,String> map = aService.chalPageCheck(cpage);
+		int currentPage = Integer.parseInt(map.get("currentPage"));
+		int start = Integer.parseInt(map.get("start"));
+		int end = Integer.parseInt(map.get("end"));
+
+		String navi = aService.getChalPageNaviSearch(currentPage,select,keyword);
+		List<ChalDTO> list = aService.selectChalAllSearch(start,end,select,keyword);
+		model.addAttribute("cpage",cpage);
+		model.addAttribute("select",select);
+		model.addAttribute("keyword",keyword);
+		model.addAttribute("navi",navi);
+		model.addAttribute("list",list);
+		return "/admin/adminChalSearch";
+	}
+
+	@RequestMapping("chalDelete") // 챌린지 관리에서 글 삭제.
+	public String chalDelete(Model model, int cpage, String select, String keyword, int[] checkbox) throws Exception {
+		for(int i=0; i<checkbox.length; i++) {
+			aService.chalDelete(checkbox[i]);
+		}
+		model.addAttribute("cpage",cpage);
+		return "redirect:/admin/chal";
+	}
+
+	@RequestMapping("chalRenew") // 챌린지 관리에서 갱신.
+	public String chalRenew(Model model, int cpage, String select, String keyword, int[] checkbox) throws Exception {
+		for(int i=0; i<checkbox.length; i++) {
+			aService.chalRenew(checkbox[i]);
+		}
+		model.addAttribute("cpage",cpage);
+		return "redirect:/admin/chal";
+	}
+
+	@RequestMapping("chalWriteForm") // 챌린지 작성 폼으로 이동.
+	public String chalWriteForm(Model model, int cpage, String select, String keyword) {
+		model.addAttribute("cpage",cpage);
+		model.addAttribute("select",select);
+		model.addAttribute("keyword",keyword);
+		return "/admin/adminChalWriteForm";
+	}
+
+	@RequestMapping("chalWrite") // 챌린지 등록.
+	public String chalWrite(String from, String to, ChalDTO dto) throws ParseException {
+		aService.insertChal(from, to, dto);
+		return "redirect:/admin/chal?cpage=1";
+	}
+	
+	@RequestMapping("chalModifyForm") // 챌린지 수정 폼으로 이동.
+	public String chalModify(Model model, int cpage, String select, String keyword, int chalSeq) throws ParseException {
+		// chalSeq로 챌린지 찾기.
+		ChalDTO list = aService.chalSearchBySeq(chalSeq);
+		model.addAttribute("cpage",cpage);
+		model.addAttribute("select",select);
+		model.addAttribute("keyword",keyword);
+		model.addAttribute("list",list);
+		return "/admin/adminChalModifyForm";
+	}
+	
+	@RequestMapping("chalModify") // 챌린지 수정.
+	public String chalModify(String from, String to, ChalDTO dto) throws ParseException {
+		aService.modifyChal(from, to, dto);
+		return "redirect:/admin/chal?cpage=1";
+	}
 
 
 	//유저 블랙관리 페이지로 이동
