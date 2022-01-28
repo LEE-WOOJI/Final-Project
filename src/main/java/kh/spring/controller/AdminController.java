1a1fa9a5a7d4814352b98a98785309831015ee12
yongjunyoo <yongjunyoo.dev@gmail.com>
2022-01-28 13:33:19 +0900

    용준 : memberDTO signUp_date추가, 관리자페이지 메인 완료

--- src/main/java/kh/spring/controller/AdminController.java
+++ src/main/java/kh/spring/controller/AdminController.java
@@ -3,7 +3,6 @@ package kh.spring.controller;
 import java.util.List;
 import java.sql.Timestamp;
 import java.text.ParseException;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,13 +30,13 @@ public class AdminController {
 
 	@Autowired
 	AdminService aService;
-	
+
 	@Autowired
 	BoardService bService;
-	
+
 	@Autowired
 	UserService uService;
-	
+
 	@Autowired
 	RefundService rService;
 
@@ -51,14 +50,26 @@ public class AdminController {
 		int chalResult = aService.getChalCount();
 		// 등급 출력.
 		AdminUtilsDTO gradeResult = aService.getGradeCount();
-
+		// 유저수 추이의 날짜 출력.
+		AdminUtilsDTO dateResult = aService.getDate();
+		// 유저수 추이의 누적 가입자 수 출력.
+		AdminUtilsDTO signUpAccumResult = aService.getSignUpAccumCount();
+		// 유저수 추이의 일일 가입자 수 출력.
+		AdminUtilsDTO signUpDailyResult = aService.getSignUpDailyCount();
+		// 등급별 결제 수 출력.
+		AdminUtilsDTO payResult = aService.getPayCountByGrade();
+		
 		model.addAttribute("memberResult",memberResult);
 		model.addAttribute("boardResult",boardResult);
 		model.addAttribute("chalResult",chalResult);
 		model.addAttribute("gradeResult",gradeResult);
+		model.addAttribute("dateResult",dateResult);
+		model.addAttribute("signUpAccumResult",signUpAccumResult);
+		model.addAttribute("signUpDailyResult",signUpDailyResult);
+		model.addAttribute("payResult",payResult);
 		return "/admin/adminMain";
 	}
-	
+
 	@RequestMapping("board") // 자유게시판 관리로 이동.
 	public String board(Model model, int cpage) throws Exception {
 		// 자유게시판 글 수 출력.
@@ -179,9 +190,9 @@ public class AdminController {
 		model.addAttribute("keyword",keyword);
 		return "/admin/adminChalWriteForm";
 	}
-	
+
 	// 챌린지 등록은 ImageController에서 구현.
-	
+
 	@RequestMapping("chalModifyForm") // 챌린지 수정 폼으로 이동.
 	public String chalModify(Model model, int cpage, String select, String keyword, int chalSeq) throws ParseException {
 		// chalSeq로 챌린지 찾기.
@@ -192,7 +203,7 @@ public class AdminController {
 		model.addAttribute("list",list);
 		return "/admin/adminChalModifyForm";
 	}
-	
+
 	// 챌린지 수정은 ImageController에서 구현.
 
 
@@ -203,20 +214,20 @@ public class AdminController {
 		model.addAttribute("list",list);
 		return "/admin/adminUserB";
 	}
-	
+
 	//유저 블랙관리 페이지 더보기
 	@ResponseBody
 	@RequestMapping(value = "userBlackMore", produces = "text/html;charset=utf8")
 	public String blackMore(int moreNum) {
 		int start = moreNum;
-	      int end = start + 4;
-	      System.out.println(end);
-	      List<MemberDTO> list = uService.selectBound(start, end); 
-	      Gson glist = new Gson(); 
-	      String result = glist.toJson(list); //json으로 변환
-	      return result;
+		int end = start + 4;
+		System.out.println(end);
+		List<MemberDTO> list = uService.selectBound(start, end); 
+		Gson glist = new Gson(); 
+		String result = glist.toJson(list); //json으로 변환
+		return result;
 	}
-	
+
 	//유저 블랙관리 페이지에서 검색
 	@RequestMapping("userBlackSearch")
 	public String blackSearch(Model model, String id, String nickname, String blacklist) {
@@ -230,7 +241,7 @@ public class AdminController {
 		model.addAttribute("blacklist",blacklist);
 		return "/admin/adminUserBS";
 	}
-	
+
 	//유저 블랙관리 페이지에서 검색하고 더보기
 	@ResponseBody
 	@RequestMapping(value = "userBlackSearchMore", produces = "text/html;charset=utf8")//유저 블랙 서치 더보기
@@ -240,19 +251,19 @@ public class AdminController {
 		System.out.println(id);
 		System.out.println(moreNum +"유저 블랙 서치들어옴");
 		Gson glist = new Gson();
-        List<MemberDTO> list = uService.searchBound(start, end, id, nickname, blacklist);
-        String result = glist.toJson(list);
-        return result;
+		List<MemberDTO> list = uService.searchBound(start, end, id, nickname, blacklist);
+		String result = glist.toJson(list);
+		return result;
 	}
-	
+
 	//유저 블랙 페이지 - 유저 탈퇴
 	@RequestMapping("userBlackDelete") 
 	public String blackOut(String id) {
 		uService.delete(id);
 		return "redirect:/admin/userBlack";
 	}
-	
-	
+
+
 	//유저 블랙 페이지 - 유저 정지
 	@RequestMapping("userBlackStop") 
 	public String blackStop(String id, String blacklist) {
@@ -263,21 +274,21 @@ public class AdminController {
 		uService.black(id, black);
 		return "redirect:/admin/userBlack";
 	}
-	
-	
+
+
 	// 유저 등급 관리 페이지로 이동
 	@RequestMapping("userGrade") 
 	public String grade(Model model) {
 		List<MemberDTO> blist = uService.userGrade("bronze");
 		List<MemberDTO> slist = uService.userGrade("silver");
 		List<MemberDTO> glist = uService.userGrade("gold");
-		
+
 		model.addAttribute("blist",blist);
 		model.addAttribute("slist",slist);
 		model.addAttribute("glist",glist);
 		return "/admin/adminUserG";
 	}
-	
+
 	//유저 등급 관리 페이지에서 검색
 	@RequestMapping("userGradeSearch")
 	public String gradeSearch(String option, String searchText, Model model) {
@@ -291,7 +302,7 @@ public class AdminController {
 		model.addAttribute("glist",glist);
 		return "/admin/adminUserG";
 	}
-	
+
 	//유저 등급 관리 페이지에서 등급 변경
 	@RequestMapping("userGradeMeta")
 	public String gradeMeta(String grade, String id) {
@@ -299,7 +310,7 @@ public class AdminController {
 		uService.GradeMeta(grade, id);
 		return "redirect:/admin/userGrade";
 	}
-	
+
 	//유저 환급 관리 페이지로 이동
 	@RequestMapping("userRefund")
 	public String refund(Model model) {
@@ -307,7 +318,7 @@ public class AdminController {
 		model.addAttribute("list",list);
 		return "/admin/adminUserR";
 	}
-	
+
 	//유저 환급 관리 - 환급(삭제)
 	@RequestMapping("userRefundGo")
 	public String refundGo(String nickname) {
@@ -316,7 +327,7 @@ public class AdminController {
 		System.out.println(result);
 		return "redirect:/admin/userRefund";
 	}
-	
+
 	//유저 환급 관리 페이지에서 더보기
 	@ResponseBody
 	@RequestMapping(value = "userRefundMore" , produces = "text/html;charset=utf8")
@@ -325,9 +336,9 @@ public class AdminController {
 		int end = start + 4;
 		System.out.println(moreNum +"유저 환급 더보기들어옴");
 		Gson glist = new Gson();
-        List<RefundDTO> list = rService.listBound(start, end);
-        String result = glist.toJson(list);
-        return result;
+		List<RefundDTO> list = rService.listBound(start, end);
+		String result = glist.toJson(list);
+		return result;
 	}
 	//유저 환급 관리 페이지에서 검색
 	@RequestMapping("userRefundSearch")
@@ -339,7 +350,7 @@ public class AdminController {
 		model.addAttribute("list",list);
 		return "/admin/adminUserRS";
 	}
-	
+
 	//유저 환급 관리 페이지에서 검색하고 더보기
 	@ResponseBody
 	@RequestMapping("userRefundSearchMore")
@@ -348,11 +359,11 @@ public class AdminController {
 		int end = start + 4;
 		System.out.println(moreNum +"유저 환급 검색 더보기들어옴");
 		Gson glist = new Gson();
-        List<RefundDTO> list = rService.listBound(start, end);
-        String result = glist.toJson(list);
-        return result;
+		List<RefundDTO> list = rService.listBound(start, end);
+		String result = glist.toJson(list);
+		return result;
 	}
-	
+
 	@ExceptionHandler(Exception.class)
 	public String exceptionHandler(Exception e) {
 		e.printStackTrace();
